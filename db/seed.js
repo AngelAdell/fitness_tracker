9eaefd0e933b2cdf0bf8897714cdf66728c848fb
Angel Adell <angeldeniseadell@gmail.com>
2024-04-11 22:51:57 -0400

    created tables and individual files for each table

--- db/seed.js
+++ db/seed.js
@@ -1,33 +1,59 @@
 const client = require('./client.js');
 
-const seedSync = async () => {
+
+const dropTables = async () => {
+  try {
+    await client.query(`
+      DROP TABLE IF EXISTS routine_activities;
+      DROP TABLE IF EXISTS routines;
+      DROP TABLE IF EXISTS activites;
+    `);
+  }
+  catch (error) {
+    console.error(error);
+  }
+};
+
+const createTables = async () => {
   try {
     await client.query(`
       CREATE TABLE activites (
         id SERIAL PRIMARY KEY,
         name VARCHAR(30) UNIQUE NOT NULL,
         description TEXT NOT NULL
-      );  
-
+      `);  
+      
+      await client.query(`
       CREATE TABLE routines (
         id SERIAL PRIMARY KEY,
         is_public boolean DEFAULT false,
         name VARCHAR(30) UNIQUE NOT NULL,
         goal TEXT NOT NULL
-      );
+      `);
 
+      await client.query(`
       CREATE TABLE routine_activities (
         id SERIAL PRIMARY KEY,
         "routine_id" INTEGER REFERENCES routines(id),
         "activity_id" INTEGER REFERENCES activites(id),
         count INTEGER
-      );
+      `);
       
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const seedSync = async () => {
+  try {
+    await dropTables();
+    // console.log('tables dropped');
+
+
+    await createTables();
   } catch (error) {
     console.error(error);
   }
 }
 
 seedSync();
-
-module.exports = seedSync;
